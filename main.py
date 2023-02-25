4fa59eea2c15b2ebbfdefa1bdfc82eef637fedf4
maxime-42 <mkayumba@student.42.fr>
2023-02-25 23:35:02 +0100

    update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 "included modules"
 from datetime import datetime
 from typing import Optional
-from fastapi import FastAPI, APIRouter, status, HTTPException
-from methodes import get_list_sensor, create_sensor_entry
-from models import NewEntry, SensorDb
+from fastapi import FastAPI, APIRouter, status
+from methodes import get_list_sensor, create_sensor_entry, calculate_people_inside
+from models import NewEntry
 
 app = FastAPI()
 
@@ -25,21 +25,6 @@ async def get_sensor_occupancy(sensor_id: str, atInstant: Optional[datetime] = N
     return calculate_people_inside(sensor_id, atInstant)
 
 
-def calculate_people_inside(sensor_id: str, at_instant: Optional[datetime] = None):
-    "return the number of people in the room"
-    if sensor_id not in SensorDb:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Sensor id '{sensor_id}' not found"
-        )
-    room_list = SensorDb[sensor_id]
-    if at_instant is None:
-        sensor_list = room_list
-    else:
-        sensor_list = [x for x in room_list if x.ts <= at_instant]
-    return {
-        "sensor": sensor_id,
-        "inside": sum(x.in_count - x.out_count for x in sensor_list)
-    }
 
 
 app.include_router(router)
--- methodes.py
+++ methodes.py
@@ -1,4 +1,7 @@
-"include"
+"methods"
+from datetime import datetime
+from typing import Optional
+from fastapi import HTTPException, status
 from models import SensorDb, NewEntry, SensorEntry
 
 def get_list_sensor() -> {str,list[str]}: # type: ignore
@@ -19,3 +22,20 @@ def create_sensor_entry(payload:NewEntry)->NewEntry:
     else:
         SensorDb[sensor_name] = [new_sensor]
     return payload
+
+
+def calculate_people_inside(sensor_id: str, at_instant: Optional[datetime] = None):
+    "return the number of people in the room"
+    if sensor_id not in SensorDb:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Sensor id '{sensor_id}' not found"
+        )
+    room_list = SensorDb[sensor_id]
+    if at_instant is None:
+        sensor_list = room_list
+    else:
+        sensor_list = [x for x in room_list if x.ts <= at_instant]
+    return {
+        "sensor": sensor_id,
+        "inside": sum(x.in_count - x.out_count for x in sensor_list)
+    }
