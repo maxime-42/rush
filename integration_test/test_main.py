039a0e096e75e2bc5abcbc936f444494f7ba57f1
maxime-42 <mkayumba@student.42.fr>
2023-03-03 21:45:27 +0100

    test integration is ok

--- integration_test/test_main.py
+++ integration_test/test_main.py
@@ -1,40 +1,66 @@
 """include"""
+import unittest
+from datetime import datetime
 from fastapi import status
 from fastapi.testclient import TestClient
+from app.models.shape_data import fake_db, SensorEntry
 from main import app
 
 
-client = TestClient(app)
+class SensorTestCase(unittest.TestCase):
+    """this class help to test every functions"""
 
-def test_hello_world():
-    """test"""
-    response = client.get("/api")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"msg": "Hello"}
+    def setUp(self):
+        """setup database"""
+        self.client = TestClient(app)
+        self.client.base_url = "http://localhost:8000/api"
 
-def test_get_sensors():
-    """test"""
-    response = client.get("http://localhost:8000/api/sensors/")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {'sensor': ['a', 'b', 'c']}
+        fake_db["a"] = [SensorEntry(ts = datetime.fromisoformat("2021-05-01T10:00:00+00:00"),\
+                        in_count=10, out_count=2)]
+        fake_db["b"] = [SensorEntry(ts = datetime.fromisoformat("2021-05-01T10:00:00+00:00"),\
+                        in_count=15, out_count=2)]
+        fake_db["c"] = [
+            SensorEntry(ts =
+                datetime.fromisoformat("2021-05-01T09:00:00+00:00"), in_count=5, out_count=0), #5
+            SensorEntry(ts =
+                datetime.fromisoformat("2021-05-01T10:00:00+00:00"), in_count=1, out_count=5), #1
+            SensorEntry(ts =
+                datetime.fromisoformat("2021-05-01T12:00:00+00:00"), in_count=1, out_count=1), #1
+            SensorEntry(ts =
+                datetime.fromisoformat("2021-05-01T16:01:00+00:00"), in_count=6, out_count=5), #2
+        ]
 
+    def tearDown(self):
+        fake_db.clear()
 
-def test_get_occpancy_at_instant():
-    """test to get occupancy at give time"""
-    url = "http://localhost:8000/api/sensors/c/occupancy?atInstant=2021-05-01T16:01:00"
-    response = client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"sensor": "c","inside": 2}
+    def test_hello_world(self):
+        """test"""
+        response = self.client.get("")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {"msg": "Hello world"}
 
-def test_get_occpancy():
-    """test occupancy"""
-    response = client.get("http://localhost:8000/api/sensors/c/occupancy")
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {'inside': 2, 'sensor': 'c'}
+    def test_get_sensors(self):
+        """test"""
+        response = self.client.get("sensors")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {'sensor': ['a', 'b', 'c']}
 
+    def test_get_occpancy_at_instant(self):
+        """test to get occupancy at give time"""
+        url = "sensors/c/occupancy?atInstant=2021-05-01T16:01:00"
+        response = self.client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {"sensor": "c","inside": 2}
 
-# def test_get_webhook():
-#     """test to insert """
-#     response = client.get("http://localhost:8000/api/sensors/c/occupancy")
-#     assert response.status_code == status.HTTP_200_OK
-#     assert response.json() == {'inside': 2, 'sensor': 'c'}
+    def test_get_occpancy(self):
+        """test occupancy"""
+        response = self.client.get("sensors/c/occupancy")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {'inside': 2, 'sensor': 'c'}
+
+    def test_get_webhook(self):
+        """test to insert """
+        json_data={"sensor":"abc","ts":"2023-10-10T13:00:00+00:00","in":3,"out":2}
+        response = self.client.post("webhook", json=json_data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.json() == json_data
