7bdc3f5ee0d18a5d027413f07afb16f90b9c452c
maxime-42 <mkayumba@student.42.fr>
2023-03-03 14:47:06 +0100

    add service

--- app/routers/sensors.py
+++ app/routers/sensors.py
@@ -1,33 +1,17 @@
 "methods"
 from typing import Dict, Optional
 from datetime import datetime
-from fastapi import APIRouter, HTTPException, status
-from app.models.shape_data import fake_db
-
+from fastapi import APIRouter
+# from app.models.shape_data import fake_db
+from app.service.service_sensors import get_list_sensor, calculate_people_inside
 router = APIRouter(prefix="/sensors")
 
 @router.get("/")
-def get_list_sensor() -> Dict[str,list[str]]:
+def service_get_list_sensor() -> Dict[str,list[str]]:
     "return list of knew sensor"
-    sensor_list : list[str] = []
-    for item in fake_db:
-        sensor_list.append(item)
-    return {"sensor" : sensor_list}
+    return get_list_sensor()
 
 @router.get("/{sensor_id}/occupancy")
-def calculate_people_inside(sensor_id: str, at_instant: Optional[datetime] = None):
-    "return the number of people in the room"
-
-    if sensor_id not in fake_db:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Sensor id '{sensor_id}' not found"
-        )
-    room_list = fake_db[sensor_id]
-    if at_instant is None:
-        sensor_list = room_list
-    else:
-        sensor_list = [x for x in room_list if x.ts <= at_instant]
-    return {
-        "sensor": sensor_id,
-        "inside": sum(x.in_count - x.out_count for x in sensor_list)
-    }
+def service_get_people_inside(sensor_id: str, at_instant: Optional[datetime] = None):
+    """return the number of people in the room"""
+    return calculate_people_inside(sensor_id, at_instant)
--- test/test_main.py
+++ test/test_main.py
@@ -7,7 +7,33 @@ client = TestClient(app)
 
 def test_read_main():
     """test"""
-    response = client.get("/")
+    response = client.get("/api")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"msg": "Hello"}
-    
+
+def test_get_sensors():
+    """test"""
+    response = client.get("http://localhost:8000/api/sensors/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'sensor': ['a', 'b', 'c']}
+
+
+def test_get_occpancy_at_instant():
+    """test to get occupancy at give time"""
+    url = "http://localhost:8000/api/sensors/c/occupancy?atInstant=2021-05-01T16:01:00"
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"sensor": "c","inside": 2}
+
+def test_get_occpancy():
+    """test occupancy"""
+    response = client.get("http://localhost:8000/api/sensors/c/occupancy")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {'inside': 2, 'sensor': 'c'}
+
+
+# def test_get_webhook():
+#     """test to insert """
+#     response = client.get("http://localhost:8000/api/sensors/c/occupancy")
+#     assert response.status_code == status.HTTP_200_OK
+#     assert response.json() == {'inside': 2, 'sensor': 'c'}
